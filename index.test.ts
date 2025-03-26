d18362dc8b34d3b3206979eb472502583bca9d6e
Shahrad Elahi <shahrad@litehex.com>
2025-03-26 05:59:05 +0000

    chore: benchmark (#14)

--- index.test.ts
+++ index.test.ts
@@ -1,14 +1,33 @@
 import { createHash } from 'node:crypto';
 import { expect } from 'chai';
+import { Bench } from 'tinybench';
 
 import { sha256 } from './index.js';
 
-describe('SHA-256', () => {
-  const data = 'This is a sample data!';
+const sample = 'This is a sample data!';
 
+describe('SHA-256', () => {
   it('should', () => {
-    const hash = Buffer.from(sha256(data)).toString('hex');
-    const nodeHash = createHash('sha256').update(data).digest('hex');
+    const hash = Buffer.from(sha256(sample)).toString('hex');
+    const nodeHash = createHash('sha256').update(sample).digest('hex');
     expect(hash).to.equal(nodeHash);
   });
+});
+
+it('Performance', async () => {
+  const bench = new Bench({ time: 100 });
+
+  bench
+    .add('@se-oss/sha256', () => {
+      sha256(sample);
+    })
+    .add('node:crypto', async () => {
+      createHash('sha256').update(sample).digest('hex');
+    });
+
+  await bench.warmup();
+  await bench.run();
+
+  // eslint-disable-next-line no-console
+  console.table(bench.table());
 });
